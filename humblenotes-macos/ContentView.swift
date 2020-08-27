8442c7e260ed5f3930068130d34a12c3794db422
Frank Chiarulli Jr <frank@frankchiarulli.com>
2020-08-26 21:34:46 -0400

    Initial Commit

---
+++ humblenotes-macos/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  humblenotes-macos
+//
+//  Created by Frank Chiarulli Jr. on 8/26/20.
+//  Copyright © 2020 Left Shift Logical, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
