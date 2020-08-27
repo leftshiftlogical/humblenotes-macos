8b68503339df83a69dc2b3cb478535b9670fe721
Frank Chiarulli Jr <frank@frankchiarulli.com>
2020-08-27 00:19:46 -0400

    cleanup

--- humblenotes-macos/AppDelegate.swift
+++ humblenotes-macos/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         let popover = NSPopover()
         popover.contentSize = NSSize(width: 400, height: 400)
-        popover.behavior = .transient // should hide popover when focus switches to another app but doesn't ...
+        popover.behavior = .transient // this should hide popover when focus switches to another app but doesn't ...
         popover.contentViewController = NSHostingController(rootView: contentView)
         self.popover = popover
         
@@ -43,7 +43,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let event = NSApp.currentEvent!
 
         if event.type == NSEvent.EventType.rightMouseUp {
-            // hack once you set a menu any click will
+            // HACK: once you set a menu any click will
             // activate it so set the menu, trigger a
             // synthetic click, then unset the menu
             // to allow other actions again
@@ -56,16 +56,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @objc func togglePopover(_ sender: AnyObject?) {
-        if let button = self.statusItem.button {
-            if self.popover.isShown {
-                self.popover.performClose(sender)
-            } else {
-                // fix for popover not closing on focus change
-                // must always be called before self.popover.show
-                // in order for applicationWillResignActive to be called
-                NSApp.activate(ignoringOtherApps: true)
-                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
-            }
+        if self.popover.isShown {
+            hidePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
+        
+    func hidePopover(_ sender: AnyObject?) {
+        self.popover.performClose(sender)
+    }
+    
+    func showPopover(_ sender: AnyObject?) {
+        if let button = statusItem.button {
+            // fix for popover not closing on focus change
+            // must always be called before self.popover.show
+            // in order for applicationWillResignActive to be called
+            NSApp.activate(ignoringOtherApps: true)
+            self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
         }
     }
     
